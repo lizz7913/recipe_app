08f94800f83be1ea1e1283a25b3db20c05befc30
mohammedhashim44 <mohammedhashim4498@gmail.com>
2022-09-13 16:50:17 +0200

    added missing Scaffold

--- lib/src/screens/recipes_screen.dart
+++ lib/src/screens/recipes_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:onboarding/models/recipe.dart';
 
@@ -11,33 +12,35 @@ class RecipesScreen extends StatelessWidget {
   const RecipesScreen({Key key,@required this.userName}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('Recipe').snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return ListView(
-          children: snapshot.data.docs.map((document) {
-            var food=Recipe.fromJson(document.data());
-            var comment=Comments.fromJson(document.data());
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: (){
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => IngredientScreen(id: food.id, user: this.userName, recipe: comment.recipe,)));
-                  },
-                    child:Text(food.name)),
-
-              ],
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('Recipe').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          }).toList(),
-        );
-      },
+          }
+          return ListView(
+            children: snapshot.data.docs.map((document) {
+              var food=Recipe.fromJson(document.data());
+              var comment=Comments.fromJson(document.data());
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: (){
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => IngredientScreen(id: food.id, user: this.userName, recipe: comment.recipe,)));
+                    },
+                      child:Text(food.name)),
+
+                ],
+              );
+            }).toList(),
+          );
+        },
+      ),
     );
 
 
